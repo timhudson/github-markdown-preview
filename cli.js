745d6fa4e1200b163b7bde61f893ea9e1f985e52
Eugene Sharygin <eush77@gmail.com>
2015-05-24 13:53:17 +0300

    Check that cb is a function in updateServerHTML

--- cli.js
+++ cli.js
@@ -68,6 +68,6 @@ function logHTML() {
 function updateServerHTML(cb) {
   generateHTML(function(err, html) {
     server.update(html)
-    if (cb) cb()
+    if (typeof cb == 'function') cb()
   })
 }
